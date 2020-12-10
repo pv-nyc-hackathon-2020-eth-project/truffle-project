9f6f28fa8968af43fb122fc2590b7da39b1810ac
O'laughlin <doolaugh@f0189814e7e8.ant.amazon.com>
2020-12-10 18:13:02 -0500

    adding literary passages for purchase

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -7,6 +7,7 @@ contract Marketplace {
 
   struct itemStruct {
     string item;
+    string author;
     uint priceInUSD;
     uint priceInCDBRA;
   }
@@ -23,27 +24,31 @@ contract Marketplace {
   uint currentCDBRADiscount;
   mapping(string => itemStruct) private itemsAndPricesMapping;
   mapping(uint => string) private itemNumberToItemName;
+  mapping(string => string) private itemToPassage;
   mapping(address => purchasedItemStruct[]) private addressToPurchases;
-
-  //address to item map expose a method
 
   constructor(address _cadabraCoinAddress) public {
     cadabraCoinAddress = _cadabraCoinAddress;
 
-    itemsAndPricesMapping["item1"] = itemStruct({item: "item1", priceInUSD: 10, priceInCDBRA: 10 * 1000});
-    itemNumberToItemName[0] = "item1";
+    itemsAndPricesMapping["The Swimmer"] = itemStruct({item: "The Swimmer", author: "John Cheever", priceInUSD: 10, priceInCDBRA: 10 * 1000});
+    itemNumberToItemName[0] = "The Swimmer";
+    itemToPassage["The Swimmer"] = "Then there was a fine noise of rushing water from the crown of an oak at his back, as if a spigot there had been turned. Then the noise of fountains came from the crowns of all the tall trees. Why did he love storms, what was the meaning of his excitement when the door sprang open and the rain wind fled rudely up the stair, why had the simple task of shutting the windows of an old house seem fitting and urgent, why did the first watery notes of a storm wind have for him the unmistakable sound of good news, cheer, glad tidings?";
 
-    itemsAndPricesMapping["item2"] = itemStruct({item: "item2", priceInUSD: 20, priceInCDBRA: 20 * 1000});
-    itemNumberToItemName[1] = "item2";
+    itemsAndPricesMapping["The Great Gatsby"] = itemStruct({item: "The Great Gatsby", author: "F. Scott Fitzgerald", priceInUSD: 20, priceInCDBRA: 20 * 1000});
+    itemNumberToItemName[1] = "The Great Gatsby";
+    itemToPassage["The Great Gatsby"] = "And as I sat there brooding on the old, unknown world, I thought of Gatsby’s wonder when he first picked out the green light at the end of Daisy’s dock. He had come a long way to this blue lawn, and his dream must have seemed so close that he could hardly fail to grasp it. He did not know that it was already behind him, somewhere back in that vast obscurity beyond the city, where the dark fields of the republic rolled on under the night.";
 
-    itemsAndPricesMapping["item3"] = itemStruct({item: "item3", priceInUSD: 30, priceInCDBRA: 30 * 1000});
-    itemNumberToItemName[2] = "item3";
+    itemsAndPricesMapping["Fahrenheit 451"] = itemStruct({item: "Fahrenheit 451", author: "Ray Bradbury", priceInUSD: 30, priceInCDBRA: 30 * 1000});
+    itemNumberToItemName[2] = "Fahrenheit 451";
+    itemToPassage["Fahrenheit 451"] = "Books were only one type of receptacle where we stored a lot of things we were afraid we might forget. There is nothing magical in them, at all. The magic is only in what books say, how they stitched the patches of the universe together into one garment for us.";
 
-    itemsAndPricesMapping["item4"] = itemStruct({item: "item4", priceInUSD: 40, priceInCDBRA: 40 * 1000});
-    itemNumberToItemName[3] = "item4";
+    itemsAndPricesMapping["To Kill a Mockingbird"] = itemStruct({item: "To Kill a Mockingbird", author: "Harper Lee", priceInUSD: 40, priceInCDBRA: 40 * 1000});
+    itemNumberToItemName[3] = "To Kill a Mockingbird";
+    itemToPassage["To Kill a Mockingbird"] = "Atticus said to Jem one day, “I’d rather you shot at tin cans in the back yard, but I know you’ll go after birds. Shoot all the bluejays you want, if you can hit ‘em, but remember it’s a sin to kill a mockingbird.” That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it. “Your father’s right,” she said. “Mockingbirds don’t do one thing but make music for us to enjoy. They don’t eat up people’s gardens, don’t nest in corncribs, they don’t do one thing but sing their hearts out for us. That’s why it’s a sin to kill a mockingbird.”";
 
-    itemsAndPricesMapping["item5"] = itemStruct({item: "item5", priceInUSD: 50, priceInCDBRA: 50 * 1000});
-    itemNumberToItemName[4] = "item5";
+    itemsAndPricesMapping["Of Mice and Men"] = itemStruct({item: "Of Mice and Men", author: "John Steinbeck", priceInUSD: 50, priceInCDBRA: 50 * 1000});
+    itemNumberToItemName[4] = "Of Mice and Men";
+    itemToPassage["Of Mice and Men"] = "As happens sometimes, a moment settled and hovered and remained for much more than a moment. And sound stopped and movement stopped for much, much more than a moment.";
 
     numberOfItems = 5;
     earningsInUSD = 0;
@@ -69,9 +74,9 @@ contract Marketplace {
     return allItemsAndPrices;
   }
 
-  function addItem(string memory _item, uint _priceInUSD) public {
+  function addItem(string memory _item, string memory _author, uint _priceInUSD) public {
     uint priceOfItemInCDBRA = (_priceInUSD * 1000) - totalCDBRADiscount;
-    itemsAndPricesMapping[_item] = itemStruct({item: _item, priceInUSD: _priceInUSD, priceInCDBRA: priceOfItemInCDBRA});
+    itemsAndPricesMapping[_item] = itemStruct({item: _item, author: _author, priceInUSD: _priceInUSD, priceInCDBRA: priceOfItemInCDBRA});
     itemNumberToItemName[numberOfItems] = _item;
     numberOfItems++;
   }
@@ -104,7 +109,7 @@ contract Marketplace {
     uint priceOfItemInUSD = itemsAndPricesMapping[_item].priceInUSD;
     uint priceOfItemInCDBRA = itemsAndPricesMapping[_item].priceInCDBRA;
     deductCDBRAFromPurchaserOfItem(priceOfItemInCDBRA);
-    addCustomer(msg.sender, _item, "sample passage");
+    addCustomer(msg.sender, _item, itemToPassage[_item]);
     //Increment marketplace earnings in USD
     earningsInUSD = earningsInUSD + priceOfItemInUSD;
     //Update all of the item prices in CDBRA now that earnings have been incremented
